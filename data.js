9ed1a1a30b91943eae4c1af35d7ba2cfbe064208
Macflash <alex.brofo@outlook.com>
2015-01-09 10:47:05 -0600

    basic gameboard setup

--- data.js
+++ data.js
@@ -1,1 +1,41 @@
 //STUB
+
+// GameBoard --
+function gameboard(countries, map, players, markers){
+	this.countries = countries;
+	this.map = map;
+	this.players = players;
+}
+
+// Country ----
+function country(name, x, y, maxVal, dev, fort){
+	this.name = name;
+	this.x = x;
+	this.y = y;
+	this.maxVal = maxVal;
+	this.dev = dev;
+	this.fort = fort;
+	this.isHabitable = true;
+}
+
+// Countries --
+var countries = [];
+
+var britain = new country("Britain",15,4,1,true);
+countries.push(britain);
+
+// Map --------
+//  A graph detailing country connections
+var map = [];
+
+// Players
+
+function player(name, bank, research){
+	this.name = name;
+	this.bank = bank;
+	this.research = research;
+}
+
+player1 = new player("player1", 100,0);
+
+var players = [];
--- main.js
+++ main.js
@@ -1,1 +1,92 @@
 //STUB
+
+var main_ui;
+var dev_ui;
+var mil_ui;
+var res_ui;
+
+var bank_ui;
+var research_ui;
+
+var game;
+
+//ideally called about once per sec
+function update(){
+	//update markers
+	updateMarkers(game,1);
+	
+	//update bank
+	updateBank(player1, 1);
+	
+	//player1.bank = player1.bank + 1;
+	setConstUI(player1);
+}
+
+function updateBank(player, time){
+	player1.bank += time;
+}
+
+function updateMarkers(player, time){
+	
+}
+
+function setConstUI(player){
+	bank_ui.innerHTML = player1.bank;
+	research_ui.innerHTML = player1.research;
+}
+
+function init(){
+	main_ui = document.getElementById("main-ui");
+	dev_ui = document.getElementById("dev-ui");
+	mil_ui = document.getElementById("mil-ui");
+	res_ui = document.getElementById("res-ui");
+	
+	dev_ui.style.display = "none";
+	mil_ui.style.display = "none";
+	res_ui.style.display = "none";
+	
+	bank_ui = document.getElementById("bank_ui");
+	research_ui = document.getElementById("research_ui");
+	
+	update();
+	setInterval(update, 1000);
+	
+	//return 0;
+}
+
+// UI navigation functions
+function home(){
+	dev_ui.style.display = "none";
+	mil_ui.style.display = "none";
+	res_ui.style.display = "none";
+	
+	main_ui.style.display = "block";
+	//return 0;
+}
+
+function dev(){
+	main_ui.style.display = "none";
+	mil_ui.style.display = "none";
+	res_ui.style.display = "none";
+	
+	dev_ui.style.display = "block";
+	//return 0;
+}
+
+function mil(){
+	main_ui.style.display = "none";
+	mil_ui.style.display = "none";
+	res_ui.style.display = "none";
+	
+	mil_ui.style.display = "block";
+	//return 0;
+}
+
+function res(){
+	main_ui.style.display = "none";
+	dev_ui.style.display = "none";
+	mil_ui.style.display = "none";
+	
+	res_ui.style.display = "block";
+	//return 0;
+}
